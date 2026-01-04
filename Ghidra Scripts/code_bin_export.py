839b81803119942c783083868e441337101be933
ZiouraS2 <marshmallowserver@gmail.com>
2026-01-04 00:35:07 -0700

    size bugfix

--- Ghidra Scripts/code_bin_export.py
+++ Ghidra Scripts/code_bin_export.py
@@ -25,14 +25,34 @@ prevaddrchecked = 0
 clear = lambda: os.system('cls')
 clear()
 
-#calc biggest string for each
+#calc biggest string for each and size table
+sizetbl = [0,]
+i = 0
 for sym in functionSymbols:
     namespace = sym.getParentNamespace().getName(True)  
     addr = sym.getAddress()
     name = sym.getName()
-    if "DAT_" not in name and "FUN_" not in name and "SUB_" not in name and "LAB_" not in name and "caseD_" not in name and "switchD" not in name and "switchdataD_" not in name:
+    type1 = "Code"
+
+    if addr.getOffset() >= 4956160:
+	type1 = "Data"
+    printstr = ""
+    if i > 0:
+        if "DAT_" not in name and "+" not in name or type1 == "Data":
+            debugprevstart = sizetbl[i]
+            sizetbl[i-1] = addr.getOffset() - sizetbl[i]
+            #printstr = ("end: "+str(addr.getOffset())+" - start: "+str(debugprevstart)+" = "+str(sizetbl[i-1]))
+    if "DAT_" not in name and "+" not in name or type1 == "Data":
+        i = i + 1
+        sizetbl.append(addr.getOffset())
+    
+    
+    if "DAT_" not in name and "FUN_" not in name and "SUB_" not in name and "LAB_" not in name and "caseD_" not in name and "switchD" not in name and "switchdataD_" not in name and "+" not in name:
 	if "s_" not in name[:3] and "u_" not in name[:3]:
 		if name != "default":
+			#print(i)        
+			#print(addr.getOffset())
+			#print(str(sym.getParentNamespace())+"::"+sym.getName()+" : "+printstr)
 			if str(sym.getParentNamespace()) != "Global" and "switchD" not in str(sym.getParentNamespace()):
     				name = str(sym.getParentNamespace())+"::"+sym.getName()
 			if len(name) > strsymbollen:
@@ -41,8 +61,7 @@ for sym in functionSymbols:
 				straddresslen = len(" @ "+str(addr.getOffset()))
 			if len(str(addr.getOffset() - prevaddrchecked)) > strsizelen:
 				strsizelen = len(str(addr.getOffset() - prevaddrchecked))
-    if "DAT_" not in name and "+" not in name:
-	prevaddrchecked = addr.getOffset()
+    
 
 #actually print stuff
 prevaddrchecked2 = 0
@@ -50,15 +69,15 @@ strsizelen = strsizelen + 2
 strtypelen = strtypelen + 2
 straccesslen = straccesslen + 2
 strsymbollen = strsymbollen + 2
-print("Size"+" "*(strsizelen-4)+"Type"+" "*(strtypelen-4)+"Access"+" "*(straccesslen-6)+"Symbol"+" "*(strsymbollen-6)+"Address"+" "*straddresslen)
+print("Size"+" "*(strsizelen-4)+"Type"+" "*(strtypelen-4)+"Access"+" "*(straccesslen-6)+"Symbol"+" "*(strsymbollen-6)+" Address"+" "*straddresslen)
 print("---------------------------------------------------------------")
 
 functionSymbols2 = symbolTable.getAllSymbols(True)
+i2 = 0
 
 for sym in functionSymbols2:
     name = sym.getName()
     addr = sym.getAddress()
-    size = addr.getOffset() - prevaddrchecked2
     type1 = "Code"
     access = "RO" 
 
@@ -66,11 +85,14 @@ for sym in functionSymbols2:
 	type1 = "Data"
     if addr.getOffset() >= 5664768:
 	access = "RW"
+    
+    if "DAT_" not in name and "+" not in name or type1 == "Data":
+        i2 = i2 + 1
+    size = sizetbl[i2]  
+    
     if "DAT_" not in name and "FUN_" not in name and "SUB_" not in name and "LAB_" not in name and "caseD_" not in name and "switchD" not in name and "switchdataD_" not in name and "+" not in name:
 	if "s_" not in name[:3] and "u_" not in name[:3]:
 		if name != "default":
 			if str(sym.getParentNamespace()) != "Global" and "switchD" not in str(sym.getParentNamespace()):
     				name = str(sym.getParentNamespace())+"::"+sym.getName()
-    			print(str(size)+" "*(strsizelen- len(str(size)) )+type1+" "*(strtypelen- len(str(type1)) )+access+" "*(straccesslen- len(str(access)) )+name+" "*(strsymbollen- len(name) )+str(" @ "+hex(addr.getOffset())).split("L")[0]+" "*(straddresslen- len(" @ "+str(addr.getOffset())) ))
-    if "DAT_" not in name and "+" not in name:
-	prevaddrchecked2 = addr.getOffset()
+    			print(str(size)+" "*(strsizelen- len(str(size)) )+type1+" "*(strtypelen- len(str(type1)) )+access+" "*(straccesslen- len(str(access)) )+name+" "*(strsymbollen- len(name) )+str(" @ "+hex(addr.getOffset())).split("L")[0]+" "*(straddresslen- len(str(addr.getOffset())) ))
